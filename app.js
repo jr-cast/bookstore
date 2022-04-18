7d605afe0913b0ccbfbdb43889ce9ac25e88547f
Brenda309 <brewihogora23@gmail.com>
2022-04-18 23:06:20 +0200

    Add function to the remove books

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ let removeBtns = document.getElementsByClassName('rmv');
 
 
 addBtn.addEventListener('click', () => {
+  let li = document.createElement('li');
+   const addTitle= document.createElement('p');
+   const addAuthor = document.createElement('p');
+   const addbutton = document.createElement('button');
+   const hr = document.createElement('hr')
+   
+   li.appendChild(addTitle,addAuthor,addButton,hr)
+console.log (li)
+
+
+      
   console.log('click')
   books[2] = {
     title: bookTitle.value,
@@ -23,9 +34,16 @@ addBtn.addEventListener('click', () => {
   }
 });
 
-for (let i = 0; i < removeBtns.length; i++) {
+for(let i = 0; i < removeBtns.length; i++) {
   removeBtns[i].addEventListener('click', () => {
-    let title = document.getElementById(' title1').innerHTML;
-    console.log(title);
+    
+    // let titleBook = document.getElementById(`title${i}`).innerHTML;
+    // let writer = document.getElementById(`author${i}`).innerHTML;
+    delete books[i]
+    // delete books[i].author
+     // console.log(title);
+    console.log(books)
   })
 }
+
+
