baaab06f0a0249f788fdefdb83fcff053c2cbb2b
jr-cast <castanos@pm.me>
2022-04-19 05:07:21 -0500

    add,remove elements and update local storage

--- app.js
+++ app.js
@@ -1,49 +1,89 @@
-const books = [
-  {
-    title: 'First Book Title',
-    author: 'Test Book1 Author'
-  },
-  {
-    title: 'Second Book Title',
-    author: 'Test Book2 Author'
-  },
-];
 
-let bookTitle = document.getElementById('title');
-let author = document.getElementById('author');
+// Book Object
+let Book = new Object();
+// Books Collection
+let Books = [];
+if (localStorage.Books) {
+  Books = JSON.parse(localStorage.Books);
+}
+// add books to collection and display them
 let addBtn = document.getElementById('addBtn');
-let removeBtns = document.getElementsByClassName('rmv');
-
-
 addBtn.addEventListener('click', () => {
-  let li = document.createElement('li');
-   const addTitle= document.createElement('p');
-   const addAuthor = document.createElement('p');
-   const addbutton = document.createElement('button');
-   const hr = document.createElement('hr')
-   
-   li.appendChild(addTitle,addAuthor,addButton,hr)
-console.log (li)
-
-
-      
-  console.log('click')
-  books[2] = {
-    title: bookTitle.value,
-    author: author.value
-  }
+  const bookTitle = document.getElementById('title');
+  const author = document.getElementById('author');
+  const book = Object.create(Book);
+  book.title = bookTitle.value;
+  book.author = author.value;
+  const ul = document.querySelector('ul');
+  const li = document.createElement('li');
+  li.style.listStyle = 'none';
+  li.id = document.getElementsByClassName('li').length;
+  const addTitle = document.createElement('p');
+  addTitle.innerHTML = book.title;
+  const addAuthor = document.createElement('p');
+  addAuthor.innerHTML = book.author;
+  const rmvButton = document.createElement('button');
+  rmvButton.innerHTML = 'Remove';
+  rmvButton.className = 'rmv';
+  rmvButton.id = document.getElementsByClassName('rmv').length;
+    // remove created button if desired (from list and from local storage)
+    rmvButton.addEventListener('click', () => {
+      let filtered = Books.filter(Books => Books.title !== book.title);
+      let stringData = JSON.stringify(filtered);
+      localStorage.setItem('Books', stringData);
+      li.remove();
+    })
+  
+  const hr = document.createElement('hr')
+  li.appendChild(addTitle);
+  li.appendChild(addAuthor);
+  li.appendChild(rmvButton);
+  li.appendChild(hr);
+  ul.appendChild(li);
+  Books.push(book);
+  bookTitle.value = author.value = '';
+  let stringData = JSON.stringify(Books);
+  localStorage.setItem('Books', stringData);
 });
 
-for(let i = 0; i < removeBtns.length; i++) {
-  removeBtns[i].addEventListener('click', () => {
-    
-    // let titleBook = document.getElementById(`title${i}`).innerHTML;
-    // let writer = document.getElementById(`author${i}`).innerHTML;
-    delete books[i]
-    // delete books[i].author
-     // console.log(title);
-    console.log(books)
-  })
-}
+// Display localStorage Books Collection
+if (localStorage.Books) {
+for (let i = 0; i < JSON.parse(localStorage.Books).length; i++) {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.id = `${i}`;
+    li.style.listStyle = 'none';
+    const addTitle= document.createElement('p');
+    addTitle.innerHTML = JSON.parse(localStorage.Books)[i].title;
+    li.appendChild(addTitle);
+    const addAuthor = document.createElement('p');
+    addAuthor.innerHTML = JSON.parse(localStorage.Books)[i].author;
+    li.appendChild(addAuthor);
+    const rmvButton = document.createElement('button');
+    rmvButton.innerHTML = 'Remove';
+    rmvButton.id = `btn${i}`;
+    rmvButton.className = 'rmv';
+    li.appendChild(rmvButton);
+    const hr = document.createElement('hr')
+    li.appendChild(hr);
+    ul.appendChild(li);
+}};
+
+// remove books from collection and display
+  for (let i = 0; i < document.getElementsByClassName('rmv').length; i++) {
+    const rmvButton = document.getElementById(`btn${i}`);
+    const titleName = Books[i].title;
+    const list = document.getElementById(`${i}`);
+    rmvButton.addEventListener('click', () => {
+      let filtered = Books.filter(Books => Books.title !== titleName && Books.author !== author);
+      let stringData = JSON.stringify(filtered);
+      localStorage.setItem('Books', stringData);
+      list.remove();
+    })
+  }
+
+
+
+
 
 
