1c21f34e278371af5773791a91dc0efe50dd1fdb
jr-cast <castanos@pm.me>
2022-04-19 11:41:47 -0500

    refactor localstorage and display functions

--- app.js
+++ app.js
@@ -1,59 +1,42 @@
-
 // Book Object
-let Book = new Object();
+const Book = {
+  title: '',
+  author: '',
+}
 // Books Collection
 let Books = [];
 if (localStorage.Books) {
   Books = JSON.parse(localStorage.Books);
 }
-// add books to collection and display them
-let addBtn = document.getElementById('addBtn');
+
+// populate local storage collection with user entries
+const addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', () => {
   const bookTitle = document.getElementById('title');
   const author = document.getElementById('author');
-  const book = Object.create(Book);
-  book.title = bookTitle.value;
-  book.author = author.value;
-  const ul = document.querySelector('ul');
-  const li = document.createElement('li');
-  li.style.listStyle = 'none';
-  li.id = document.getElementsByClassName('li').length;
-  const addTitle = document.createElement('p');
-  addTitle.innerHTML = book.title;
-  const addAuthor = document.createElement('p');
-  addAuthor.innerHTML = book.author;
-  const rmvButton = document.createElement('button');
-  rmvButton.innerHTML = 'Remove';
-  rmvButton.className = 'rmv';
-  rmvButton.id = document.getElementsByClassName('rmv').length;
-    // remove created button if desired (from list and from local storage)
-    rmvButton.addEventListener('click', () => {
-      let filtered = Books.filter(Books => Books.title !== book.title);
-      let stringData = JSON.stringify(filtered);
-      localStorage.setItem('Books', stringData);
-      li.remove();
-    })
-  
-  const hr = document.createElement('hr')
-  li.appendChild(addTitle);
-  li.appendChild(addAuthor);
-  li.appendChild(rmvButton);
-  li.appendChild(hr);
-  ul.appendChild(li);
-  Books.push(book);
-  bookTitle.value = author.value = '';
-  let stringData = JSON.stringify(Books);
-  localStorage.setItem('Books', stringData);
+  if (!bookTitle.value || !author.value) {
+    alert('Enter both book title and author please');
+  } else {
+    const book = Object.create(Book);
+    book.title = bookTitle.value;
+    book.author = author.value;
+    Books.push(book);
+    bookTitle.value = '';
+    author.value = '';
+    let stringData = JSON.stringify(Books);
+    localStorage.setItem('Books', stringData);
+    window.location.reload();
+  }
 });
 
-// Display localStorage Books Collection
+// Display localStorage Books Collection entries
 if (localStorage.Books) {
-for (let i = 0; i < JSON.parse(localStorage.Books).length; i++) {
+  for (let i = 0; i < JSON.parse(localStorage.Books).length; i += 1) {
     const ul = document.querySelector('ul');
     const li = document.createElement('li');
     li.id = `${i}`;
     li.style.listStyle = 'none';
-    const addTitle= document.createElement('p');
+    const addTitle = document.createElement('p');
     addTitle.innerHTML = JSON.parse(localStorage.Books)[i].title;
     li.appendChild(addTitle);
     const addAuthor = document.createElement('p');
@@ -67,20 +50,23 @@ for (let i = 0; i < JSON.parse(localStorage.Books).length; i++) {
     const hr = document.createElement('hr')
     li.appendChild(hr);
     ul.appendChild(li);
-}};
-
-// remove books from collection and display
-  for (let i = 0; i < document.getElementsByClassName('rmv').length; i++) {
-    const rmvButton = document.getElementById(`btn${i}`);
-    const titleName = Books[i].title;
-    const list = document.getElementById(`${i}`);
-    rmvButton.addEventListener('click', () => {
-      let filtered = Books.filter(Books => Books.title !== titleName && Books.author !== author);
-      let stringData = JSON.stringify(filtered);
-      localStorage.setItem('Books', stringData);
-      list.remove();
-    })
   }
+};
+
+// Remove books from collection and display
+for (let i = 0; i < document.getElementsByClassName('rmv').length; i += 1) {
+  const rmvButton = document.getElementById(`btn${i}`);
+  const titleName = Books[i].title;
+  const list = document.getElementById(`${i}`);
+  rmvButton.addEventListener('click', () => {
+    let filtered = Books.filter(Books => Books.title !== titleName);
+    console.log(filtered);
+    let stringData = JSON.stringify(filtered);
+    localStorage.setItem('Books', stringData);
+    list.remove();
+    window.location.reload();
+  })
+}
 
 
 
